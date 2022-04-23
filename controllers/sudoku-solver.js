286fd39fbcdb9ad671d4dbdb8f0e2e726fc1db49
Jesus Damken <jesusdmk@gmail.com>
2022-04-22 23:46:54 -0600

    Refactoring

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,25 +1,6 @@
 "use strict";
 
 class SudokuSolver {
-  
-  createRegion(x, y) {
-    const puzzlePos = [],
-          matrix    = [];
-
-    [x, y].forEach(el => {
-      if (el % 3 == 0) matrix.push([el, el + 1, el + 2]);
-      if (el % 3 == 1) matrix.push([el - 1, el, el + 1]);
-      if (el % 3 == 2) matrix.push([el - 2, el - 1, el]);
-    });
-
-    const [rows, cols] = matrix;
-
-    rows.forEach(r => {
-      cols.forEach(c => puzzlePos.push((r * 9) + c));
-    });
-
-    return puzzlePos;
-  }
 
   validate(puzzleString) {
     const rule = /[^1-9\.]+/i;
@@ -38,45 +19,20 @@ class SudokuSolver {
     }
   }
 
-  checkValue(value) {
-    return parseInt(value) >= 1 && parseInt(value) <= 9;
-  }
-
-  checkCoordinate(coordinate) {
-    if (coordinate.length != 2) return false;
-    
-    const xy   = coordinate.toLowerCase(),
-          rule = /^[a-z][0-9]$/i,
-          rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i"],
-          cols = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-    const validations = [
-      rows.includes(xy[0]),
-      cols.includes(xy[1]),
-      rule.test(xy)
-    ];
-
-    const testCoordinate = validations.every(v => v == true);
-
-    if (!testCoordinate) return false;
-    
-    return { rowIndex: rows.indexOf(xy[0]), colIndex: cols.indexOf(xy[1]) };
-  }
-
-  checkRowPlacement(puzzleString, row, column, value) {
-    const { rowIndex } = this.checkCoordinate(row + column),
-            rowStart   = rowIndex * 9,
-            rowEnd     = rowStart + 9,
-            rowValues  = puzzleString.slice(rowStart, rowEnd);
+  checkRowPlacement(puzzleString, row, value) {
+    const rows      = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+    const rowStart  = rows.indexOf(row.toLowerCase()) * 9;
+    const rowEnd    = rowStart + 9;
+    const rowValues = puzzleString.slice(rowStart, rowEnd);
 
     return !rowValues.includes(value);
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
-    const { colIndex } = this.checkCoordinate(row + column);
+  checkColPlacement(puzzleString, column, value) {
+    const cols = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
     let colValues = "",
-        colStart  = colIndex;
+        colStart  = cols.indexOf(column);
 
     for (let i = 0; i < 9; i++) {
       colValues += puzzleString[colStart];
@@ -87,12 +43,29 @@ class SudokuSolver {
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-    const { rowIndex, colIndex } = this.checkCoordinate(row + column),
-                         indexes = this.createRegion(rowIndex, colIndex);
+    const matrix = [];
+    const region = [];
+    const rows   = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+    const cols   = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    const rowIndex = rows.indexOf(row.toLowerCase());
+    const colIndex = cols.indexOf(column);
 
     let regionValues = "";
 
-    indexes.forEach(i => regionValues += puzzleString[i]);
+    [rowIndex, colIndex].forEach(el => {
+      if (el % 3 === 0) matrix.push([el, el + 1, el + 2]);
+      if (el % 3 === 1) matrix.push([el - 1, el, el + 1]);
+      if (el % 3 === 2) matrix.push([el - 2, el - 1, el]);
+    });
+
+    const [rowsMatrix, colsMatrix] = matrix;
+
+    rowsMatrix.forEach(r => {
+      colsMatrix.forEach(c => region.push((r * 9) + c));
+    });
+
+    region.forEach(i => regionValues += puzzleString[i]);
 
     return !regionValues.includes(value);
   }
@@ -100,11 +73,21 @@ class SudokuSolver {
   checkConflicts(puzzleString, row, column, value) {
     const conflict = [];
 
-    if (!this.checkRowPlacement(puzzleString, row, column, value)) {
+    const rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+    const cols = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    const rowIndex = rows.indexOf(row.toLowerCase());
+    const colIndex = cols.indexOf(column);
+
+    if (puzzleString[(rowIndex * 9) + colIndex] == value) {
+      return { valid: true };
+    }
+
+    if (!this.checkRowPlacement(puzzleString, row, value)) {
       conflict.push("row");
     }
 
-    if (!this.checkColPlacement(puzzleString, row, column, value)) {
+    if (!this.checkColPlacement(puzzleString, column, value)) {
       conflict.push("column");
     }
 
@@ -113,22 +96,34 @@ class SudokuSolver {
     }
     
     if (conflict.length > 0) {
-      return { valid: false, conflict }
+      return { valid: false, conflict };
     } else {
-      return { valid: true }
+      return { valid: true };
     }
+    
   }
 
   solve(puzzleString) {
     
     // Starting point
-    let splitPuzzle = puzzleString.split("");
+    const splitPuzzle = puzzleString.split("");
+    const rowsIndexes = [];
+    const colsIndexes = [];
+    const regsIndexes = [];
+
     let checkSpaces = splitPuzzle.includes(".");
 
     // Define starting indexes for rows, cols and regions
-    const rowIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    const colIndex = rowIndex.map(el => el * 9);
-    const regIndex = rowIndex.slice(0, 3).map(el => [el * 9, el * 9 + 1, el * 9 + 2]).flat();
+    const firstRow    = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    const firstCol    = firstRow.map(el => el * 9);
+    const firstRegion = firstRow.slice(0, 3).map(el => [el * 9, el * 9 + 1, el * 9 + 2]).flat();
+
+    // Map all indexes
+    for (let i = 0; i < 9; i++) {
+      rowsIndexes.push(firstRow.map(r => (i * 9) + r));
+      colsIndexes.push(firstCol.map(c => c + i));
+      regsIndexes.push(firstRegion.map(m => (Math.floor(i / 3) * 18) + (i * 3) + m));
+    }
 
     // Sudoku resolver
     do {
@@ -137,51 +132,40 @@ class SudokuSolver {
       checkSpaces = splitPuzzle.includes(".");
 
       // Define rows, cols and regions
-      const rows    = [];
-      const cols    = [];
-      const regions = [];
-      const answers = [];
+      const rowsValues = [];
+      const colsValues = [];
+      const regsValues = [];
+      const answers    = [];
 
-      // Fetch row & col values
-      for (let i = 0; i < 9; i++) {
-        let row = rowIndex.map(r => (i * 9) + r);
-        rows.push(row.reduce((prev, curr) => { return prev + splitPuzzle[curr] }, ""));
-      
-        let col = colIndex.map(c => c + i);
-        cols.push(col.reduce((prev, curr) => { return prev + splitPuzzle[curr] }, ""));
-      
-        let reg = regIndex.map(m => (Math.floor(i / 3) * 18) + (i * 3) + m);
-        regions.push(reg.reduce((prev, curr) => { return prev + splitPuzzle[curr] }, ""));
-      }
+      // Fetch row, col & region values
+      rowsIndexes.forEach(r => rowsValues.push(r.reduce((prev, curr) => { return prev + splitPuzzle[curr]; }, "")));
+      colsIndexes.forEach(c => colsValues.push(c.reduce((prev, curr) => { return prev + splitPuzzle[curr]; }, "")));
+      regsIndexes.forEach(r => regsValues.push(r.reduce((prev, curr) => { return prev + splitPuzzle[curr]; }, "")));
 
       // Calculate answer
       for (let value = 1; value <= 9; value++) {
         
-        regions.forEach((reg, regIndex) => {
+        regsValues.forEach((reg, regIndex) => {
           if (reg.includes(value)) return;
 
           const possibleAnswers = [];
           
           for (let i = 0; i < 3; i++) {
-            
-            const currentRow = rows[Math.floor(regIndex / 3) * 3 + i];
-            
+            const currentRow = rowsValues[Math.floor(regIndex / 3) * 3 + i];
+
             if (currentRow.includes(value)) continue;
 
             for (let j = 0; j < 3; j++) {
 
-              const currentCol = cols[(regIndex % 3) * 3 + j];
+              const currentCol = colsValues[(regIndex % 3) * 3 + j];
 
               if (currentCol.includes(value)) continue;
 
-              const position = (i * 3) + j;
+              const intersection   = (i * 3) + j;
+              const puzzlePosition = ((Math.floor(regIndex / 3) * 3 + i) * 9) + (((regIndex % 3) * 3) + j);
 
-              if (!parseInt(reg[position])) {
-                possibleAnswers.push({
-                  validValue: value,
-                  stringPos: ((Math.floor(regIndex / 3) * 3 + i) * 9) + (((regIndex % 3) * 3) + j)
-                });
-              }
+              if (!parseInt(reg[intersection])) possibleAnswers.push({ value, puzzlePosition });
+
             }
 
             answers.push(possibleAnswers);
@@ -189,13 +173,13 @@ class SudokuSolver {
         });
       }
 
-      const uniqueAnswers = answers.filter(el => el.length == 1).flat();
+      const uniqueAnswers = answers.filter(el => el.length === 1).flat();
 
-      if (uniqueAnswers.length == 0) break;
+      if (uniqueAnswers.length === 0) break;
 
-      uniqueAnswers.forEach(ans => splitPuzzle[ans.stringPos] = `${ans.validValue}`);
+      uniqueAnswers.forEach(ans => splitPuzzle[ans.puzzlePosition] = `${ans.value}`);
       
-    } while (checkSpaces)
+    } while (checkSpaces);
 
     return (!checkSpaces) ? { solution: splitPuzzle.join("") } : { error: "Puzzle cannot be solved" };
   }
--- routes/api.js
+++ routes/api.js
@@ -7,53 +7,86 @@ module.exports = function (app) {
   let solver = new SudokuSolver();
 
   app.route('/api/check')
-    .post((req, res) => {
-      const { puzzle, coordinate, value } = req.body;
-     
-      if (!puzzle || !coordinate || !value) {
+    .post((req, res, next) => {
+      // Middleware - Check for missing fields
+      if (!req.body.puzzle || !req.body.coordinate || !req.body.value) {
         return res.json({ error: "Required field(s) missing" });
       }
-      
-      const [ row, col ]  = coordinate;
-      const checkPuzzle   = solver.validate(puzzle);
-      const checkCoord    = solver.checkCoordinate(coordinate);
-      const checkValue    = solver.checkValue(value);
-      const checkConflict = solver.checkConflicts(puzzle, row, col, value);
 
-      if (!checkPuzzle.isValid) {
-        return res.json({ error: checkPuzzle.error })
+      next();
+
+    }, (req, res, next) => {
+      // Middleware - Check for value between 1 and 9
+      const { value } = req.body;
+
+      const validation = [ parseInt(value) >= 1, parseInt(value) <= 9];
+
+      if (!validation.every(v => v == true)) {
+        return res.json({ error: "Invalid value" });
       }
 
-      if (!checkCoord) {
+      next();
+
+    }, (req, res, next) => {
+      // Middleware - Check for valid puzzle string
+      let { isValid, error } = solver.validate(req.body.puzzle);
+
+      if (!isValid) return res.json({ error });
+
+      next();
+
+    }, (req, res, next) => {
+      // Middleware - Check for valid coordinate
+      const { coordinate } = req.body;
+
+      const xy = coordinate.toLowerCase(),
+          rule = /^[a-z][0-9]$/i,
+          rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i"],
+          cols = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+      const validations = [
+            rows.includes(xy[0]),
+            cols.includes(xy[1]),
+            rule.test(xy),
+            coordinate.length == 2];
+      
+      if (!validations.every(v => v == true)) {
         return res.json({ error: "Invalid coordinate" });
       }
 
-      if (!checkValue) {
-        return res.json({ error: "Invalid value" });
-      }
+      next();
+
+    }, (req, res) => {
+      const { puzzle, coordinate, value } = req.body;
+      const [ row, col ]  = coordinate;
+
+      const checkConflict = solver.checkConflicts(puzzle, row, col, value);
 
       return res.json(checkConflict);
 
     });
     
+
   app.route('/api/solve')
-    .post((req, res) => {
-      const { puzzle }  = req.body;
-      const checkPuzzle = solver.validate(puzzle);
+    .post((req, res, next) => {
       
-      if (!req.body.puzzle) {
-        return res.json({ error: "Required field missing" });
-      }
+      // Middleware - Check for required field
+      
+      if (!req.body.puzzle) return res.json({ error: "Required field missing" });
 
-      if (!checkPuzzle.isValid) {
-        return res.json({ error: checkPuzzle.error });
-      }
+      next();
+    }, (req, res, next) => {
+      
+      // Middleware - Check for valid puzzle string
+      
+      const { isValid, error } = solver.validate(req.body.puzzle);
 
-      console.log("All good");
+      if (!isValid) return res.json({error});
 
-      const answer = solver.solve(puzzle);
-
+      next();
+    } , (req, res) => {
+      const answer = solver.solve(req.body.puzzle);
+      
       return res.json(answer);
-
     });
 };
